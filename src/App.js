3d217577f192933223ffb75c4cb5eed3bd8ae6c8
Jonathan Tuala <jetuala@gmail.com>
2022-01-03 20:09:47 -0800

    Experimenting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { scrapeSubreddit } from './scraper';
+import ArticleList from './ArticleList';
 
 function App() {
   return (
-    <div className="App">
+    <div className="UATU">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Uatu: a Marvel Reddit web-scraping stock market game.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <ArticleList />
     </div>
   );
 }
